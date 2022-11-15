dd3dd65af6e888e54d5602fac2b32c8d3ca88620
RalphDePaz <202080468@psu.palawan.edu.ph>
2022-11-15 10:55:38 +0800

    commit

--- projectsite/baseball/admin.py
+++ projectsite/baseball/admin.py
@@ -1,9 +1,6 @@
-
-# Register your models here.
 from django.contrib import admin
 from django.contrib.admin import display
 from .models import Position, Person, Club, Play
-
 
 
 # Register your models here.
@@ -14,16 +11,15 @@ class PositionAdmin(admin.ModelAdmin):
     search_fields = ("description",)
 
 @admin.register(Person)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ("lastname", "firstname", "height", "weight",)
-    search_fields = ("lastname", "firstname", "height", "weight",)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("lastname","firstname","height","weight")
+    search_fields = ("lastname",)
 
 @admin.register(Club)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ("name", "coach", "dorm_latitude", "dorm_longitude",)
-    search_fields = ("name", "coach", "dorm_latitude", "dorm_longitude",)
-    
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ("name","coach",)
+    search_fields = ("name",)
+
 @admin.register(Play)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ("player", "team", "string_no", "pos","isActive",)
-    search_fields = ("player", "team", "string_no", "pos","isActive",)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ("player","team","string_no","isActive",)
--- projectsite/baseball/models.py
+++ projectsite/baseball/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class BaseModel(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         abstract = True
 
@@ -15,12 +14,13 @@ class Position(BaseModel):
 class Person(BaseModel):
     lastname = models.CharField(max_length=100)
     firstname = models.CharField(max_length=100)
-    height = models.DecimalField(max_digits=10, decimal_places=5, null=True, blank=True)
-    weight = models.DecimalField(max_digits=10, decimal_places=5, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True, max_length=100)
+    height = models.DecimalField(max_digits=10, decimal_places=5, null=True)
+    weight = models.DecimalField(max_digits=10, decimal_places=5, null=True)
 
 class Club(BaseModel):
     name = models.CharField(max_length=100)
-    coach = models.CharField(max_length=100)
+    coach = models.ForeignKey(Person, on_delete=models.CASCADE)
     dorm_latitude = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
     dorm_longitude = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
 
@@ -33,3 +33,4 @@ class Play(BaseModel):
     string_no = models.CharField(max_length=100, choices = STRING_CHOICES)
     isActive = models.BooleanField(default=False)
     pos = models.ForeignKey(Position, on_delete=models.CASCADE)
+
